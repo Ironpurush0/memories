21f256cce330fe427e75bfc1e6dcc98cebdee50f
Ironpurush0 <ridamsaxena90.com>
2020-11-08 16:04:09 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,12 @@
-console.log("server")
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const cross = require('cors')
+
+const app = express()
+
+const PORT = 5000
+
+app.listen(PORT, () => {
+    console.log("Server is running.")
+})
