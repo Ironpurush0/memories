b2e001582a8ec2e0c7ac560d4debb845b6d6d8f4
Ironpurush0 <ridamsaxena90.com>
2020-11-09 21:59:27 +0530

    adding redux.

---
+++ frontend/src/components/Posts/Posts.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Post from './Post/Post'
+
+import useStyles from './Styles'
+
+const Posts = () => {
+    const classes = useStyles()
+    return (
+        <>
+            <Post />
+        </>
+    )
+}
+
+export default Posts
