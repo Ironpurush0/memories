a027ddfe84d7eec479dc07c43c3427a6a5f19837
Ironpurush0 <ridamsaxena90.com>
2020-11-10 12:21:08 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,26 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const cross = require('cors')
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import mongoose from 'mongoose'
+
+import postRouter from './routes/posts.js'
 
 const app = express()
 
-const PORT = 5000
+app.use('/posts', postRouter)
 
-app.listen(PORT, () => {
+app.use(bodyParser.json({limit: "30mb", extended: true}))
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
+app.use(cors())
+
+const CONNECTION_URL = 'mongodb+srv://tonystark:QGrBYHfEl7DUi1Wv@cluster0.qbrbf.mongodb.net/test?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000
+
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => app.listen(PORT, () => {
     console.log("Server is running.")
-})
+}))
+.catch((error) => console.log(error.message) )
+
+mongoose.set('useFindAndModify', false)
