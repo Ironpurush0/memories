04d72e240eb286746a26f83624db7d4611528c14
Ironpurush0 <ridamsaxena90.com>
2020-11-12 22:59:20 +0530

    adding the routes.

--- backend/index.js
+++ backend/index.js
@@ -7,11 +7,11 @@ import postRouter from './routes/posts.js'
 
 const app = express()
 
-app.use('/posts', postRouter)
-
 app.use(bodyParser.json({limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
 app.use(cors())
+
+app.use('/posts', postRouter)
 
 const CONNECTION_URL = 'mongodb+srv://tonystark:QGrBYHfEl7DUi1Wv@cluster0.qbrbf.mongodb.net/test?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000
